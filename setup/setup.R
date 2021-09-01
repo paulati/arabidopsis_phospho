42980db2d09823750e055008425512e870cabec4
paulati <paulati@gmail.com>
2021-09-01 16:56:25 -0300

    fix

--- setup/setup.R
+++ setup/setup.R
@@ -1,3 +1,7 @@
+# to run R 4.0.3:
+# conda activate arabidopsis_test
+# sudo /home/paulati/anaconda3/envs/arabidopsis_test/bin/R
+
 R.version
 #conda install -c conda-forge r-base=4.0.3
 
@@ -7,35 +11,6 @@ BiocManager::install("RSQLite")
 BiocManager::install("GO.db", version = '3.12')
 BiocManager::install("GOSemSim")
 BiocManager::install("topGO")
-
-# to run R 4.0.3:
-# conda activate arabidopsis_test
-# sudo /home/paulati/anaconda3/envs/arabidopsis_test/bin/R
-
-
-install.packages("httr")
-install.packages("curl")
-
-
-install.packages("XML")
-
-BiocManager::install("BiocFileCache")
-
-
-
 BiocManager::install("biomaRt")
 BiocManager::install("ViSEAGO")
-
-
-
-In .inet_warning(msg) :
-  installation of package ‘curl’ had non-zero exit status
-2: In .inet_warning(msg) :
-  installation of package ‘XML’ had non-zero exit status
-3: In .inet_warning(msg) :
-  installation of package ‘httr’ had non-zero exit status
-4: In .inet_warning(msg) :
-  installation of package ‘BiocFileCache’ had non-zero exit status
-5: In .inet_warning(msg) :
-  installation of package ‘biomaRt’ had non-zero exit status
 
